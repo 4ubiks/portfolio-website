f23fe51d1923b55af911894745e5f6db3a825c8f
4ubiks <jaharris20@liberty.edu>
2024-09-09 08:12:06 -0400

    updated the timeline, removed GNTV from history.

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -9,8 +9,21 @@ export default function Timeline(){
                 <div className="timeline-middle">
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                 </div>
+                <div className="timeline-start md:text-end mb-10 lg:pl-96">
+                <time className="font-mono italic">9/2024 - Current</time>
+                <div className="text-lg font-black">Ground Station Lead - Liberty Rocketry</div>
+                Managing the development and deployment of a ground station computer for Liberty Rocketry, a collegiate competition team ranked ninth in the world. Working closely with system and software engineers to communicate live data and video transmission for our rocket. The ground station package displays live information such as altitude, temperature and humidity logs, and velocity. A live video feed is also sent out from our rocket, which is received and displayed by the ground station package. This opportunity deals heavily with computer systems, operating system configuration, GUI application development, and hardware communication. Time is spent working in teams, collaborating, and researching effective methods to ensure all parts of our rocket are the best they can be.
+                </div>
+                <hr />
+            </li>   
+
+            <li>
+                <hr />
+                <div className="timeline-middle">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+                </div>
                 <div className="timeline-end mb-10 lg:pr-96">
-                <time className="font-mono italic">8/2023 - Current</time>
+                <time className="font-mono italic">8/2023 - 9/2024</time>
                 <div className="text-lg font-black">Lighting Technician - Liberty University Tower Theater</div>
                 Responsible for reading, analyzing, and rigging light plots for two theater spaces - a 640 seat auditorium and a smaller, more intimate Black Box theater. Responsible for hanging gel, gobo, and lens tubes for conventional fixtures and setting larger, moving-head fixtures. Also assigns power and/or DMX channels to fixtures appropriately. Sets up and wires haze machines and other visual effects fixtures, all completed in a timely manner. Occasionally works as the Light Board Operator for shows, and learned in-depth theory behind DMX, artNet, sACN, and different types of DMX configuration.
                 </div>
@@ -23,9 +36,9 @@ export default function Timeline(){
                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                 </div>
                 <div className="timeline-start md:text-end mb-10 lg:pl-96">
-                <time className="font-mono italic">5/2024 - 6/2024</time>
-                <div className="text-lg font-black">Production Intern - GNTV Media</div>
-                Spent extensive time traveling across the United States parttaking in the setup, operation, and teardown of large-scale events. Built truss lines, hung and addressed lighting fixtures, and assisted in the configuration of professional sound systems. Throughout events, acted as the Video Director and worked on setting up and operating handheld and PTZ cameras. Got first-hand experience with traveling for production work, coordinating trucks to venues, and developing a stronger understanding of how to dynamically optimize venue equipment transportation. 
+                <time className="font-mono italic">7/2024 - 8/2024</time>
+                <div className="text-lg font-black">Technical Services Intern - ACT Entertainment</div>
+                Worked closely with ACT Entertainment hardware and software technicians, servicing stage lighting products from internationally-based companies such as Ayrton and MA Lighting. Developed a strong understanding of computer hardware communication, delicate soldering, and computer systems. Spent time working within grandMA3 software, where I expanded on skills such as lighting programming, lighting design, Lua programming, and computer networking. Shopped out equipment for new customers, serviced products for existing customers, and got to demo new products before public deployment. 
                 </div>
                 <hr />
             </li> 
